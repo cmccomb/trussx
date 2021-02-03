d9ab80b7b3951ecaf22f8ac5db814be867315886
Chris McComb <ccmcc2012@gmail.com>
2021-02-03 17:58:05 -0500

    More infrastructure

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 [![Crates.io](https://img.shields.io/crates/v/trussx.svg)](https://crates.io/crates/trussx)
 [![docs.rs](https://docs.rs/trussx/badge.svg)](https://docs.rs/trussx)
 # About
-This is a work in progress
+This package provides utilities for designing and analyzing truss structures
+
+# Installation
+Install through ``crates.io`` with:
+```shell script
+cargo install trussx
+```
+
+Then add it to your ``Cargo.toml` with:
+```toml
+[dependencies]
+trussx = "0.1.0"
+```
+and add this to your root:
+```rust
+use trussx;
+```
+# Usage
+Here are some basic examples of usage
+## Building a truss
+Coming soon!
+## Analyzing a truss
+Coming soon!
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,131 @@
-enum StructuralShape {
-    Pipe,
+//! This package provides utilities for designing and analyzing truss structures
 
+// We use petgraph to make things easier
+use petgraph::Graph;
+
+/// This enum contains different structural shapes
+pub enum StructuralShape {
+    Pipe {
+        radius: f64,
+        thickness: f64,
+    },
+    IBeam {
+        width: f64,
+        height: f64,
+        web_thickness: f64,
+        flange_thickness: f64,
+    },
+    BoxBeam {
+        width: f64,
+        height: f64,
+        thickness: f64,
+    },
 }
 
-struct Node {
-    id: usize,
+struct Joint {
     x: f64,
     y: f64,
     z: f64,
+    reaction: Vec<bool>,
+    load: Vec<f64>,
 }
 
 struct Member {
-    id: usize,
-    beginning_node: usize,
-    ending_node: usize,
     cross_section: StructuralShape,
+    elastic_modulus: f64,
+    yield_strength: f64,
 }
 
-struct Load {
-
+/// This is the truss object that contains all of the necessary information about trusses
+pub struct Truss {
+    graph: petgraph::Graph<Joint, Member>,
 }
 
-struct Truss {
-    nodes: Vec<Node>,
-    members: Vec<Member>,
-    loads: Vec<Load>,
+impl Truss {
+    /// This function instantiates an empty truss
+    pub fn new() -> Truss {
+        Truss {
+            graph: petgraph::Graph::new(),
+        }
+    }
+
+    /// This function creates a new joint
+    pub fn add_joint(&mut self, x: f64, y: f64, z: f64) -> petgraph::graph::NodeIndex {
+        self.graph.add_node(Joint {
+            x,
+            y,
+            z,
+            reaction: vec![false, false, false],
+            load: vec![0.0; 3],
+        })
+    }
+
+    /// This function creates a new member to connect two joints
+    pub fn add_edge(
+        &mut self,
+        a: petgraph::graph::NodeIndex,
+        b: petgraph::graph::NodeIndex,
+        cross_section: StructuralShape,
+        elastic_modulus: f64,
+        yield_strength: f64,
+    ) -> petgraph::graph::EdgeIndex {
+        self.graph.add_edge(
+            a,
+            b,
+            Member {
+                cross_section,
+                elastic_modulus,
+                yield_strength,
+            },
+        )
+    }
+
+    /// This function moves a joint
+    pub fn move_joint(&mut self, a: petgraph::graph::NodeIndex) {
+        unimplemented!()
+    }
+
+    /// This function deletes a joint
+    pub fn delete_joint(&mut self, a: petgraph::graph::NodeIndex) {
+        unimplemented!()
+    }
+
+    /// This function deletes a member
+    pub fn delete_member(&mut self, ab: petgraph::graph::EdgeIndex) {
+        unimplemented!()
+    }
+
+    fn calculate_member_forces(&mut self) {
+        unimplemented!()
+    }
+
+    fn calculate_member_stress(&mut self) {
+        unimplemented!()
+    }
+
+    /// This function calculates the forces in each member and outputs a report
+    pub fn evaluate(&mut self) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        use crate::*;
+        let mut x = Truss::new();
+        let a = x.add_joint(0.0, 0.0, 0.0);
+        let b = x.add_joint(3.0, 0.0, 0.0);
+        let ab = x.add_edge(
+            a,
+            b,
+            StructuralShape::Pipe {
+                radius: 1.0,
+                thickness: 0.1,
+            },
+            0.0,
+            0.0,
+        );
+    }
 }
