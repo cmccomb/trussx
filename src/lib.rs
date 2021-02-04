49030c6a9c4d0918fe2e55bb58bd032969e2bdfb
Chris McComb <ccmcc2012@gmail.com>
2021-02-04 08:19:10 -0500

    Now depends on structural shape library

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,6 @@
 //! This package provides utilities for designing and analyzing truss structures
 
-/// This enum contains different structural shapes
-#[derive(Clone, Copy)]
-pub enum StructuralShape {
-    Pipe {
-        outer_radius: f64,
-        thickness: f64,
-    },
-    IBeam {
-        width: f64,
-        height: f64,
-        web_thickness: f64,
-        flange_thickness: f64,
-    },
-    BoxBeam {
-        width: f64,
-        height: f64,
-        thickness: f64,
-    },
-}
-
-impl StructuralShape {
-    fn moment_of_inertia(&self) -> f64 {
-        match self {
-            StructuralShape::Pipe { .. } => 0.0,
-            StructuralShape::IBeam { .. } => 0.0,
-            StructuralShape::BoxBeam { .. } => 0.0,
-        }
-    }
-
-    fn area(&self) -> f64 {
-        match *self {
-            StructuralShape::Pipe {
-                outer_radius,
-                thickness,
-            } => {
-                std::f64::consts::PI
-                    * (outer_radius.powf(2.0) - (outer_radius - thickness).powf(2.0))
-            }
-            StructuralShape::IBeam {
-                width,
-                height,
-                web_thickness,
-                flange_thickness,
-            } => width * height - (height - 2.0 * flange_thickness) * (width - web_thickness),
-            StructuralShape::BoxBeam {
-                width,
-                height,
-                thickness,
-            } => width * height - (width - thickness) * (height - thickness),
-        }
-    }
-}
+use structural_shapes::StructuralShape;
 
 struct Joint {
     position: [f64; 3],
