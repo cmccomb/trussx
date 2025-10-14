b2c420e1a6c0a64e12b8fafa45f4258a7ec7cf47
Chris McComb <ccmcc2012@gmail.com>
2025-10-14 17:37:27 -0400

    Validate member properties

--- examples/factor_of_safety.rs
+++ examples/factor_of_safety.rs
@@ -6,13 +6,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let joint_a = truss.add_joint(point(0.0, 0.0, 0.0));
     let joint_b = truss.add_joint(point(1.0, 0.0, 0.0));
-    truss.set_support(joint_a, [true, true, true]);
-    truss.set_support(joint_b, [false, true, true]);
-    truss.set_load(joint_b, Vector3::new(-5_000.0, 0.0, 0.0));
+    truss.set_support(joint_a, [true, true, true])?;
+    truss.set_support(joint_b, [false, true, true])?;
+    truss.set_load(joint_b, Vector3::new(-5_000.0, 0.0, 0.0))?;
 
     let member_ab = truss.add_member(joint_a, joint_b);
-    truss.set_member_properties(member_ab, 0.005, 210.0e9);
-    truss.set_member_yield_strength(member_ab, 250.0e6);
+    truss.set_member_properties(member_ab, 0.005, 210.0e9)?;
+    truss.set_member_yield_strength(member_ab, 250.0e6)?;
 
     truss.evaluate()?;
 
