fbac36f63114b97ecd67b90f450b881ed5d8c43a
Chris McComb <ccmcc2012@gmail.com>
2025-10-17 19:08:35 -0400

    Updating examples

--- examples/factor_of_safety.rs
+++ examples/factor_of_safety.rs
@@ -1,25 +1,33 @@
 use trussx::{force, point, Truss};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a simple truss with two joints and one member
     let mut truss = Truss::new();
 
+    // Define joints
     let joint_a = truss.add_joint(point(0.0, 0.0, 0.0));
     let joint_b = truss.add_joint(point(1.0, 0.0, 0.0));
+
+    // Define supports and loads
     truss.set_support(joint_a, [true, true, true])?;
     truss.set_support(joint_b, [false, true, true])?;
     truss.set_load(joint_b, force(-5_000.0, 0.0, 0.0))?;
 
+    // Define member properties
     let member_ab = truss.add_member(joint_a, joint_b);
     truss.set_member_properties(member_ab, 0.005, 210.0e9)?;
     truss.set_member_yield_strength(member_ab, 250.0e6)?;
 
+    // Evaluate the truss and unwrap the result
     truss.evaluate()?;
 
+    // Retrieve and print the factor of safety for member AB
     if let Some(factor_of_safety) = truss.member_factor_of_safety(member_ab) {
         println!("Member AB factor of safety: {factor_of_safety:.2}");
     } else {
         println!("Factor of safety is unavailable for member AB.");
     }
 
+    // All done
     Ok(())
 }
--- examples/simple.rs
+++ examples/simple.rs
@@ -1,20 +1,30 @@
 use trussx::{force, point, Truss};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a simple truss with two joints and one member
     let mut truss = Truss::new();
+
+    // Define joints
     let a = truss.add_joint(point(0.0, 0.0, 0.0));
     let b = truss.add_joint(point(1.0, 0.0, 0.0));
+
+    // Define supports and loads
     truss.set_support(a, [true, true, true])?;
     truss.set_support(b, [false, true, true])?;
     truss.set_load(b, force(-1000.0, 0.0, 0.0))?;
+
+    // Define member properties
     let ab = truss.add_member(a, b);
     truss.set_member_properties(ab, 0.01, 200.0e9)?;
 
+    // Evaluate the truss and unwrap the result
     truss.evaluate()?;
 
+    // Retrieve and print the displacement at joint B
     if let Some(displacement) = truss.joint_displacement(b) {
         println!("ux = {:.3e} m", displacement.x);
     }
 
+    // All done
     Ok(())
 }
